2d8891af19bc8fa2b1df1ba26ffbc102c49d59d4
muscleboy <1076146831@qq.com>
2019-04-11 20:50:33 +0800

    取得二维数组最大值

--- src/com/jack/how2j/array/getMaxFrom2DArray.java
+++ src/com/jack/how2j/array/getMaxFrom2DArray.java
@@ -4,9 +4,25 @@ package com.jack.how2j.array;
  * Create with IDEA.
  *
  * @Package: com.jack.how2j.array
- * @Description:
+ * @Description: 取得二维数组最大值
  * @Date: 2019/4/11 20:36
  * @Author: Wyj
  */
 public class getMaxFrom2DArray {
+    public static void main(String[] args) {
+
+        int[][] a = new int[5][5];
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                a[i][j] = (int) (Math.random() * 100);
+            }
+        }
+// 输出
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+        }
+    }
 }
