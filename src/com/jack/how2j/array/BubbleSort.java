29868269a6dad06b2fcb3bfb854e0f22ab4812d6
muscleboy <1076146831@qq.com>
2019-04-11 19:28:13 +0800

    冒泡排序

---
+++ src/com/jack/how2j/array/BubbleSort.java
@@ -0,0 +1,39 @@
+package com.jack.how2j.array;
+
+/**
+ * Create with IDEA.
+ *
+ * @Package: com.jack.how2j.array
+ * @Description:
+ * @Date: 2019/4/11 19:14
+ * @Author: Wyj
+ */
+public class BubbleSort {
+    public static void main(String[] args) {
+
+        int[] a = new int[5];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = (int) (Math.random() * 100);
+        }
+
+        for (int item : a) {
+            System.out.print(item + " ");
+        }
+
+        System.out.println();
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length - i - 1; j++) {
+                if (a[j] > a[j + 1]){
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int item : a
+             ) {
+            System.out.print(item + " ");
+        }
+    }
+}
---
+++ src/com/jack/how2j/array/selectSorted.java
@@ -0,0 +1,12 @@
+package com.jack.how2j.array;
+
+/**
+ * Create with IDEA.
+ *
+ * @Package: com.jack.how2j.array
+ * @Description:
+ * @Date: 2019/4/11 09:59
+ * @Author: Wyj
+ */
+public class selectSorted {
+}
