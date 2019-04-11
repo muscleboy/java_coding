f34ce00d0f212bc001edd290dffb0b8ac89bd44f
muscleboy <1076146831@qq.com>
2019-04-11 20:21:14 +0800

    数组填充,复制

--- src/com/jack/how2j/array/arrayCopy.java
+++ src/com/jack/how2j/array/arrayCopy.java
@@ -1,5 +1,7 @@
 package com.jack.how2j.array;
 
+import java.util.Arrays;
+
 /**
  * Create with IDEA.
  *
@@ -9,4 +11,18 @@ package com.jack.how2j.array;
  * @Author: Wyj
  */
 public class arrayCopy {
+    public static void main(String[] args) {
+
+        int[] a = new int[5];
+        Arrays.fill(a, 5);
+
+        int[] a1 = new int[5];
+
+        System.arraycopy(a, 0, a1, 0, a.length);
+
+        for (int item : a1
+             ) {
+            System.out.print(item + " ");
+        }
+    }
 }
