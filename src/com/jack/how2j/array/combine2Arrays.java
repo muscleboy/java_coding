65f2d1fa28514a91862bcc88c58a14c27c5a5637
muscleboy <1076146831@qq.com>
2019-04-11 20:49:56 +0800

    数组合并

--- src/com/jack/how2j/array/combine2Arrays.java
+++ src/com/jack/how2j/array/combine2Arrays.java
@@ -4,9 +4,44 @@ package com.jack.how2j.array;
  * Create with IDEA.
  *
  * @Package: com.jack.how2j.array
- * @Description:
+ * @Description: 数组合并
  * @Date: 2019/4/11 20:23
  * @Author: Wyj
  */
 public class combine2Arrays {
+    public static void main(String[] args) {
+
+        int[] a = new int[5];
+        int[] a1 = new int[4];
+        int[] a2 = new int[a.length + a1.length];
+
+        System.out.println("数组a: ");
+        for (int i = 0; i < a.length; i++) {
+            a[i] = (int) (Math.random() * 100);
+        }
+        for (int item : a
+             ) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+
+        System.out.println("数组a1: ");
+        for (int i = 0; i < a1.length; i++) {
+            a1[i] = (int) (Math.random() * 100);
+        }
+        for (int item : a1
+        ) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+
+        System.out.println("复制到数组a2: ");
+        System.arraycopy(a, 0, a2, 0, a.length);
+        System.arraycopy(a1, 0, a2, a1.length, a1.length);
+
+        for (int item : a2
+             ) {
+            System.out.print(item + " ");
+        }
+    }
 }
