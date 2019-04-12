9948c81b392661b651c119a85a10b09f97ee2cb0
muscleboy <1076146831@qq.com>
2019-04-12 19:37:45 +0800

    数字复制

--- src/com/jack/how2j/array/arrayCopy.java
+++ src/com/jack/how2j/array/arrayCopy.java
@@ -1,6 +1,7 @@
 package com.jack.how2j.array;
 
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  * Create with IDEA.
@@ -14,15 +15,24 @@ public class arrayCopy {
     public static void main(String[] args) {
 
         int[] a = new int[5];
-        Arrays.fill(a, 5);
+        for (int i = 0; i < a.length; i++) {
+            a[i] = new Random().nextInt(100);
+        }
+
 
         int[] a1 = new int[5];
 
         System.arraycopy(a, 0, a1, 0, a.length);
+        System.out.println(Arrays.toString(a1));
+//        for (int item : a1) {
+//            System.out.print(item + " ");
+//        }
 
-        for (int item : a1
-             ) {
-            System.out.print(item + " ");
-        }
+        int[] a2 = Arrays.copyOfRange(a, 0, 3);
+        System.out.println(Arrays.toString(a2));
+//        for (int item : a2) {
+//            System.out.print(item + " ");
+//        }
+
     }
 }
