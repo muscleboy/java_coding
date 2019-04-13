43cc25a6f4d678b0b37ea4b6c61206a59399458f
muscleboy <1076146831@qq.com>
2019-04-13 11:27:41 +0800

    二维数组排序

--- src/com/jack/how2j/array/sort2DArrays.java
+++ src/com/jack/how2j/array/sort2DArrays.java
@@ -23,18 +23,56 @@ public class sort2DArrays {
             }
         }
 
-//        二维数组 --> 一维数组
-        for (int i = 0; i < a.length; i++) {
-            System.arraycopy(a[i], 0, b, i * a[i].length, a[i].length);
-        }
-
-        Arrays.sort(b);
-//        一维数组 --> 二维数组
-        for (int i = 0; i < a.length; i++) {
-            System.arraycopy(b, i * a[i].length, a[i], 0, a[i].length);
-        }
         for (int[] item : a) {
             System.out.println(Arrays.toString(item));
         }
+
+//        二维数组选择排序
+
+        for (int i = 0; i < a.length; i++) { //5
+            for (int j = 0; j < a[i].length; j++) { // 8
+                for (int k = 0; k < a[i].length - 1; k++) {
+                    if (a[i][k] > a[i][j]){
+                        int temp = a[i][k];
+                        a[i][k] = a[i][j];
+                        a[i][j] = temp;
+                    }
+                }
+            }
+        }
+
+//        二维数组冒泡排序
+
+//        for (int i = 0; i < a.length; i++) {
+//            for (int j = 0; j < a[i].length; j++) {
+//                for (int k = 0; k < a[i].length- 1; k++) {
+//                    if (a[i][k] < a[i][k + 1]){
+//                        int temp = a[i][k];
+//                        a[i][k] = a[i][k + 1];
+//                        a[i][k + 1] = temp;
+//                    }
+//                }
+//            }
+//        }
+
+        System.out.println();
+
+        for (int[] item : a) {
+            System.out.println(Arrays.toString(item));
+        }
+
+////        二维数组 --> 一维数组
+//        for (int i = 0; i < a.length; i++) {
+//            System.arraycopy(a[i], 0, b, i * a[i].length, a[i].length);
+//        }
+//
+//        Arrays.sort(b);
+////        一维数组 --> 二维数组
+//        for (int i = 0; i < a.length; i++) {
+//            System.arraycopy(b, i * a[i].length, a[i], 0, a[i].length);
+//        }
+//        for (int[] item : a) {
+//            System.out.println(Arrays.toString(item));
+//        }
     }
 }
