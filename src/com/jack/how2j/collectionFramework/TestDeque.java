f682935e85a36bd1d2eef9966f1cca0bc1644999
muscleboy <1076146831@qq.com>
2019-04-18 21:05:34 +0800

    deque use

--- src/com/jack/how2j/collectionFramework/TestDeque.java
+++ src/com/jack/how2j/collectionFramework/TestDeque.java
@@ -1,5 +1,7 @@
 package com.jack.how2j.collectionFramework;
 
+import java.util.LinkedList;
+
 /**
  * Create with IDEA.
  *
@@ -9,4 +11,25 @@ package com.jack.how2j.collectionFramework;
  * @Author: Wyj
  */
 public class TestDeque {
+
+    public static void main(String[] args) {
+
+        LinkedList<hero> deque = new LinkedList<>();
+
+        deque.addLast(new hero("hero1"));
+        deque.addLast(new hero("hero2"));
+        deque.addLast(new hero("hero3"));
+
+        System.out.println(deque);
+
+        deque.addFirst(new hero("hexoX"));
+//        System.out.println(ll.getFirst());
+//        System.out.println(ll.getLast());
+        System.out.println(deque);
+
+        // 取出元素，会导致元素被删除
+        System.out.println(deque.removeFirst());
+        System.out.println(deque);
+    }
+
 }
