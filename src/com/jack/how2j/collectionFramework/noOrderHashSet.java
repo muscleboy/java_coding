2a8087629409bcee6f26d0d80be1b31870e17464
muscleboy <1076146831@qq.com>
2019-04-21 17:14:48 +0800

    hashset没有按元素的插入顺序排序

--- src/com/jack/how2j/collectionFramework/noOrderHashSet.java
+++ src/com/jack/how2j/collectionFramework/noOrderHashSet.java
@@ -1,12 +1,24 @@
 package com.jack.how2j.collectionFramework;
 
+import java.util.HashSet;
+
 /**
  * Create with IDEA.
  *
  * @Package: com.jack.how2j.collectionFramework
- * @Description:
+ * @Description: hashset没有按元素的插入顺序排序
  * @Date: 2019/4/21 17:11
  * @Author: Wyj
  */
 public class noOrderHashSet {
+
+    public static void main(String[] args) {
+
+        HashSet<Integer> hs = new HashSet<>();
+        hs.add(5);
+        hs.add(3);
+        hs.add(7);
+
+        System.out.println(hs);
+    }
 }
