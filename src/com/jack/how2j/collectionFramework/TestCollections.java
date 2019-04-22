adf52ef07b0c2c7ad85ee2617e6f23ee747bfa14
muscleboy <1076146831@qq.com>
2019-04-22 17:21:22 +0800

    collections的基本方法

--- src/com/jack/how2j/collectionFramework/TestCollections.java
+++ src/com/jack/how2j/collectionFramework/TestCollections.java
@@ -1,12 +1,47 @@
 package com.jack.how2j.collectionFramework;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Create with IDEA.
  *
  * @Package: com.jack.how2j.collectionFramework
- * @Description:
+ * @Description: Collestions的基本方法
  * @Date: 2019/4/22 16:36
  * @Author: Wyj
  */
 public class TestCollections {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+
+        list.add(1042);
+        list.add(1143);
+        list.add(1354);
+        list.add(1676);
+        list.add(18);
+        list.add(111);
+
+        // 反转
+//        Collections.reverse(list);
+
+        // 混淆
+//        Collections.shuffle(list);
+
+        //排序
+//        Collections.sort(list);
+
+        // 交换两个位置的值
+//        Collections.swap(list, 0, 1);
+
+        // 把list的数据，向右滚动
+//        Collections.rotate(list, 3);
+
+        System.out.println(list);
+        // 线程安全list
+        List<Integer> synchonizedlist = (List<Integer>) Collections.synchronizedList(list);
+    }
 }
---
+++ src/com/jack/how2j/collectionFramework/comparationForOrder.java
@@ -0,0 +1,12 @@
+package com.jack.how2j.collectionFramework;
+
+/**
+ * Create with IDEA.
+ *
+ * @Package: com.jack.how2j.collectionFramework
+ * @Description:
+ * @Date: 2019/4/22 17:20
+ * @Author: Wyj
+ */
+public class comparationForOrder {
+}
